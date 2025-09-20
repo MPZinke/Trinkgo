184c954595f7bd13a06dcc6b2d7300eff945fd45
mpzinke <mathew.zinke@gmail.com>
2025-09-20 18:16:22 -0500

    Second working kindof

--- source/webapp/router/__init__.py
+++ source/webapp/router/__init__.py
@@ -17,7 +17,8 @@ __author__ = "MPZinke"
 from pathlib import Path
 
 
-from flask import redirect, render_template, Flask
+from flask import redirect, render_template, request, Flask
+import requests
 
 
 WEBAPP_DIRECTORY = Path(__file__).parents[1]
@@ -25,6 +26,8 @@ HTML_DIRECTORY = WEBAPP_DIRECTORY / "html"
 STATIC_DIRECTORY = WEBAPP_DIRECTORY / "static"
 
 print(HTML_DIRECTORY)
+
+BEARER_TOKEN = None
 
 
 app = Flask("Catan", template_folder=HTML_DIRECTORY, static_folder=STATIC_DIRECTORY)
@@ -38,12 +41,31 @@ def authenticate():
 
 @app.route("/authenticated")
 def authenticated():
-	return render_template("authenticated.j2")
+	global BEARER_TOKEN
+
+	code = request.args.get("code")
+	if(code is None):
+		raise Exception("URL parameter `code` is missing.")
+
+	url = "https://accounts.spotify.com/api/token"
+	headers = {
+		"Authorization": "Basic OGRjN2Y4Yjc1NzkzNGQ5ZWJhZjM5ZjkzNDdiY2NjNTY6N2U5YzQyMTI3OWI3NGQ2OWE2YTliNTBlNDkzY2ZhOGU=",
+		"Content-Type": "application/x-www-form-urlencoded",
+	}
+	params = {
+		"code": code,
+		"grant_type": "authorization_code",
+		"redirect_uri": "http://127.0.0.1:8080/authenticated",
+	}
+	response = requests.post(url, headers=headers, params=params)
+	BEARER_TOKEN = response.json().get("access_token")
+	return redirect("/home")
 
 
-# @app.route("/callback")
-@app.route("/home")
-def home():
-	return render_template("home.j2")
+@app.get("/")
+@app.get("/home")
+def GET_home():
+	if BEARER_TOKEN is None:
+		return redirect("/authenticate")
 
-
+	return render_template("home.j2", bearer_token=BEARER_TOKEN)
